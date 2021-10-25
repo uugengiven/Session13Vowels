341f039129056ae6318104025b4176bc867546c1
John Lange <lange.john.m@gmail.com>
2021-10-25 14:05:05 -0400

    Loops over all of the arguments and prints them

---
+++ VowelCounter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VowelCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"{i}) {args[i]}");
+            }
+        }
+    }
+}
